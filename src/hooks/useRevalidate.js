5addab56577aa6aec6a2cb985b77c5e1a4026604
factsfinder <phanindrasrikar@gmail.com>
2023-11-05 23:40:48 +0530

    fix revalidate call

--- src/hooks/useRevalidate.js
+++ src/hooks/useRevalidate.js
@@ -7,13 +7,12 @@ export default function useRevalidate() {
     }
     try {
       // Todo: use server actions when it is stable and then make use of secrets using dynamic import and sst config.
-      await axios.post(
-        `/revalidate`,
-        {
-          tag,
-        },
-        { withCredentials: true }
-      );
+      await axios({
+        method: "post",
+        url: `/api/revalidate`,
+        data: { tag },
+        withCredentials: true,
+      });
     } catch (error) {
       console.log(error);
     }
